2e3490edde3190e79d7938e44eea8d9d079e8bde
Casey Selby <caseyjselby@gmail.com>
2022-04-16 16:30:09 -0700

    Add Homework

---
+++ Week2Homework.js
@@ -0,0 +1,13 @@
+let pizzaPlace = 'Pizza Pals';
+let numberOfToppings = 8;
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+console.log(`My mom says, "You can find the best pizza at ${pizzaPlace}. They have ${numberOfToppings} toppings to choose from!"`)
+
+if (numberOfToppings < 10) {
+    console.log("Quality, not quantity.")
+} else {
+    console.log("A whole lot of pizza.")
+}
