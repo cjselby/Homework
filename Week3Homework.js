4a8acacbd16a8b7a24ccd493bde16f75c29ac233
Casey Selby <caseyjselby@gmail.com>
2022-04-27 15:27:52 -0700

    Add hwk

--- Week3Homework.js
+++ Week3Homework.js
@@ -1,24 +1,63 @@
-let pizzaToppings = ["Pepperoni", "Mushroom", "Pineapple", "Onion"];
+//#1
+const pizzaToppings = ["Pepperoni", "Mushroom", "Pineapple", "Onion"];
 
-function greetCustomer(pizzaPlace, pizzaToppings) {
-  console.log(`Welcome to ${pizzaPlace}, our toppings are ${pizzaToppings}`);
+//#2
+function greetCustomer(toppings) {
+  let greeting = "Welcome, our toppings are";
+  for (let topping of toppings) {
+    greeting += `${topping},`;
+  };
+  console.log(greeting);
 };
-greetCustomer("Pizza Pals", "Pepperoni, Mushroom, Pineapple, Onion");
+greetCustomer(pizzaToppings);
 
+//#3
 function getPizzaOrder(size, crust, ...toppings) {
-  console.log(`One ${size} ${crust} pizza with ${toppings} coming up!`);
+  let order = `One ${size} ${crust} pizza with `;
+  for(let topping of toppings){
+    order += `${topping}`
+    order += `coming up`
+  };
+  console.log(order);
+  return [size, crust, toppings];
 };
 getPizzaOrder("large", "thick", ["pepperoni, mushroom, pineapple, onion"]);
 
-function preparePizza([size, crust, toppings],) {
-  console.log(`Your ${[size, crust, toppings]} pizza is cooking!`);
-}
-preparePizza(["large", "thick crust", "pepperoni mushroom pineapple onion"]);
+//#4
+function preparePizza([size, crust, toppings]) {
+  let prepareMessage = "Cooking pizza";
+  const pizzaObject = {
+    size: "large",
+    crust: "thick",
+    toppings: ["pepperoni",
+              "mushroom",
+              "pineapple",
+              "onion"]
+  };
+  console.log(prepareMessage);
+  return (pizzaObject);
+};
+preparePizza("large", "thick", "pepperoni", "mushroom", "pineapple", "onion");
 
-function servePizza({size, crust, toppings}) {
-  let pizzaObject = {size: "large", crust: "thick", toppings: "pepperoni"}
-  console.log(`Order up! Here's your ${size} ${crust} with ${toppings}. Enjoy!`);
-}
-servePizza("large", "thick", "pepperoni");
+
+//#5
+
+const anotherPizzaObject = {
+  size: "large",
+  crust: "thick",
+  toppings: ["pepperoni",
+              "mushroom",
+              "pineapple",
+              "onion"]
+};
+function servePizza(zaObject) {
+  let readyMessage = `Order up! Here's your ${zaObject.size} ${zaObject.crust} with`;
+  for(let topping of zaObject.toppings) {
+    readyMessage += `${topping},`
+  };
+  console.log(readyMessage);
+  return anotherPizzaObject;
+};
+servePizza(anotherPizzaObject);
 
 
